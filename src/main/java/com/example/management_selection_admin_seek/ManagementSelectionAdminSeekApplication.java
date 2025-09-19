ff63c8fbb6db9d4155a81efa2ef1667c4ef03995
GabrielSB19 <gasuarez2002@gmail.com>
2025-09-18 22:33:04 -0500

    first commit

---
+++ src/main/java/com/example/management_selection_admin_seek/ManagementSelectionAdminSeekApplication.java
@@ -0,0 +1,13 @@
+package com.example.management_selection_admin_seek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSelectionAdminSeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementSelectionAdminSeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/management_selection_admin_seek/ManagementSelectionAdminSeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.management_selection_admin_seek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSelectionAdminSeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
